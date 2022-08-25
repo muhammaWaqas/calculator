04e37ece92b110670fbee44cfeb342e90fd8bde4
Muhammad-Waqas <112065584+muhammaWaqas@users.noreply.github.com>
2022-08-25 12:24:58 +0500

    Add files via upload

---
+++ Calculator/app.js
@@ -0,0 +1,12 @@
+function calc(b){
+    var a = document.getElementById('ooo')
+    a.value += b
+}
+function result(){
+    var z = document.getElementById('ooo')
+    z.value = eval(z.value)
+}
+function cut (){
+    var w = document.getElementById('ooo')
+    w.value = ''
+}
